8e044c7ad3e47257b63305bc1cc1c79efdcb3f1e
oljapriya <poljako@gmail.com>
2021-07-05 21:26:39 -0400

    create env file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require ('./controllers');
 const sequelize = require ('./config/connection');
 const path = require ('path');
 
-const helpers = require ('./utils/helpers');
+const helpers = require ('./utils/helper');
 
 const exphbs = require ('express-handlebars');
 const hbs = exphbs.create({ helpers });
